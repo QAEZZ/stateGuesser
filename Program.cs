1dbebed32589a79142f69724f2e6c54f7d30d3fc
QAEZZ <russian.devv@gmail.com>
2023-03-21 08:28:22 -0500

    init

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+namespace stateGuesser;
+
+class Program {
+    public static void Main() {
+        Random rand = new Random();
+        Console.WriteLine("Shit state guesser gaem");
+
+        //string test = "ok";
+
+        dynamic? stateObj = JsonConvert.DeserializeObject(File.ReadAllText("./states.json"));
+
+        int amount = stateObj?.amount - 1;
+        int stateIndex = rand.Next(0, amount);
+        string? state = stateObj?.states[stateIndex].state;
+        string? st = stateObj?.states[stateIndex].stereotype;
+
+        Console.WriteLine($"Stereotype:\n{st}\n");
+        Console.Write("What state is this? >>> ");
+        string? guess = Console.ReadLine();
+
+        if (guess?.ToLower() == state) {
+            Console.WriteLine($"you are correct!\nState: {state}");
+        } else {
+            Console.WriteLine($"you are wrong!\nState: {state}");
+        }
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# shit state guesser gaem
+
+I was bored...
+
+guess a game via a stereotype. Only 3 states now because I have better things to work on.
